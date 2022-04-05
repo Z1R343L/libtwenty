5ac697994e9f1e0ec85ea1c90db151a25b8770d6
Z1R343L <jan2705g@gmail.com>
2022-04-05 20:48:46 +0200

    2.0.0

--- libtwenty/__init__.py
+++ libtwenty/__init__.py
@@ -1,17 +1,13 @@
-"""
-[2048 lib]
-
-"""
+import itertools
 import secrets
 from copy import deepcopy
 from os.path import abspath
 from pathlib import Path
-from typing import Optional, Literal, Union
+from typing import Optional, Union
 
 import numpy as np
 from numpy.random import choice
 from PIL import Image, ImageDraw, ImageFont
-import srsly
 
 move_dict = {"up": 0, "right": 1, "down": 2, "left": 3}
 
@@ -21,16 +17,26 @@ tile_radius = 20
 assets_path = Path(abspath(__file__)).parent / "assets"
 font = ImageFont.truetype(str(assets_path / "AGAALER.TTF"), 52, encoding="unic")
 
-t_colors = srsly.read_yaml(assets_path / "t_colors.yaml")
+t_colors = {
+    0: 0xFFA0B0BD,
+    2: 0xFFDAE4EE,
+    4: 0xFFCEE1EE,
+    8: 0xFF7EB2F4,
+    16: 0xFF6B97F6,
+    32: 0xFF697EF7,
+    64: 0xFF4861F7,
+    128: 0xFF90D6EE,
+    256: 0xFF84D3EE,
+    512: 0xFF78D1EC,
+    1024: 0xFF3FC5ED,
+    2048: 0xFF2DC2ED
+}
 
 t_range = list(t_colors.keys())
 
 
 def font_color(tile: int) -> int:
-    if tile in {2, 4}:
-        return 0xFF656E77
-    else:
-        return 0xFFF1F6F8
+    return 0xFF656E77 if tile in {2, 4} else 0xFFF1F6F8
 
 
 def prep_tiles() -> dict:
@@ -57,28 +63,24 @@ tiles = prep_tiles()
 
 
 def stack(board) -> None:
-    for i in range(0, len(board)):
-        for j in range(0, len(board)):
-            k = i
-            while board[k][j] == 0:
-                if k == len(board) - 1:
-                    break
-                k += 1
-            if k != i:
-                board[i][j], board[k][j] = board[k][j], 0
+    for i, j in itertools.product(range(len(board)), range(len(board))):
+        k = i
+        while board[k][j] == 0 and k != len(board) - 1:
+            k += 1
+        if k != i:
+            board[i][j], board[k][j] = board[k][j], 0
 
 
 def sum_up(board) -> None:
-    for i in range(0, len(board) - 1):
-        for j in range(0, len(board)):
-            if board[i][j] != 0 and board[i][j] == board[i + 1][j]:
-                board[i][j] += board[i + 1][j]
-                board[i + 1][j] = 0
+    for i, j in itertools.product(range(len(board) - 1), range(len(board))):
+        if board[i][j] != 0 and board[i][j] == board[i + 1][j]:
+            board[i][j] += board[i + 1][j]
+            board[i + 1][j] = 0
 
 
 def spawn_tile(board) -> np.ndarray:
     zeroes_flatten = np.where(board == 0)
-    zeroes_indices = [(x, y) for x, y in zip(zeroes_flatten[0], zeroes_flatten[1])]
+    zeroes_indices = list(zip(zeroes_flatten[0], zeroes_flatten[1]))
     random_index = zeroes_indices[choice(len(zeroes_indices), 1)[0]]
     board[random_index] = secrets.choice([2, 2, 4])
     return board
@@ -90,57 +92,43 @@ class Board:
         size: int = 4,
         state_string: Optional[str] = None
     ) -> None:
-        """
-        [2048 board]
-        Args:
-            size (int, optional): [board size]. Defaults to 4.
-        """
-        self.board = np.zeros((size, size), int)
+        self.score, self.possible_moves = [None] * 2
+        self.size = size
+        self.board = np.zeros((self.size, self.size), int)
         if state_string:
             self.from_state_string(state_string=state_string)
-        self.board = spawn_tile(board=self.board)
-        self.board = spawn_tile(board=self.board)
-        self.score = self.board.sum()
+        else:
+            self.board = spawn_tile(board=self.board)
+            self.board = spawn_tile(board=self.board)
+        self.calculate_score()
         self.update_possible_moves()
-        self.size = size
 
-    def board_string(self) -> str:
+    def __str__(self) -> str:
         return str(self.board)
 
     def to_state_string(self) -> str:
-        return '_'.join([str(t_range[i]) for i in np.nditer(self.board)])
+        return ''.join(f'{i:2d}' for i in np.nditer(self.board))
     
     def from_state_string(self, state_string: str) -> None:
-        row, current_row = [0] * 2
-        for i in state_string.split('_'):
-            self.board[row][current_row] = t_range[int(i)]
-            if current_row == 3:
-                current_row = 0
-                row += 1
-            else:
-                current_row += 1
-
-
+        self.board = np.reshape([int(state_string[i : i + 2]) for i in range(0, 32, 2)], (4, 4))
 
     def render(self) -> Image:
-
         image_size = tile_size * self.size
         im = Image.new(
             "RGB",
             (image_size + (tile_outline * 2), image_size + (tile_outline * 2)),
             0x8193A4,
         )
-        for x in range(self.size):
-            for y in range(self.size):
-                im_t = tiles[self.board[x][y]]
-                y1, x1 = tile_size * x, tile_size * y
-                im.paste(im=im_t, box=(x1 + tile_outline, y1 + tile_outline), mask=im_t)
+        for x, y in itertools.product(range(self.size), range(self.size)):
+            im_t = tiles[self.board[x][y]]
+            y1, x1 = tile_size * x, tile_size * y
+            im.paste(im=im_t, box=(x1 + tile_outline, y1 + tile_outline), mask=im_t)
         return im
 
 
     def move(
         self, 
-        action: Union[int, Literal[list(move_dict.keys())]],
+        action: Union[int, str],
         evaluate: bool = False
     ) -> bool:
         board_copy = deepcopy(self.board)
@@ -159,12 +147,6 @@ class Board:
         return True
 
     def update_possible_moves(self) -> None:
-        """
-        [evaluates which move directions can succeeed]
-
-        Returns:
-            dict: [dict with the results]
-        """
         res, n, over = {}, 0, False
         for direction in ["left", "right", "up", "down"]:
             res[direction] = self.move(action=direction, evaluate=True)
